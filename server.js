42d4ec20b6c3f42d1dc8a3040752180f5052ec56
ark222858 <ark222858@gmail.com>
2016-09-28 08:51:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,89 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-    'article-one':{
-     title:'Article one|Aakriti',
-     heading:'Article one',
-     date:'sep 5 2016',
-     content:`
-          <p> 
-            this is article one 
-            </p>`
-    },
-    'article-two':{
-     title:'Article two|Aakriti',
-     heading:'Article two',
-     date:'sep 5 2016',
-     content:`
-           <p> 
-           this is article two
-           </p>`
-    },
-    
-    'article-three':{
-     title:'Article three|Aakriti',
-     heading:'Article three',
-     date:'sep 5 2016',
-     content:`
-           <p> 
-           this is article three 
-           </p>`
-    }
-};
-
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-    
-    var htmlTemplate=`
-    
-    <html>
-    <head>
-        <title>
-         ${title}
-        </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-             ${date}
-        </div>
-        <div>
-            <p>
-                ${content}
-            </p>
-           
-        </div>
-        </div>
-        
-    </body>
-    
-</html>
-`;
-return htmlTemplate;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
-app.get('/:articleName',function(req,res){
-    var articleName=reg.prarams.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
 
 
 
