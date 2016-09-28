10b1c6657bcb30d3da8e630b57bdd942a1be9fac
ark222858 <ark222858@gmail.com>
2016-09-28 09:26:51 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/:articleName',function (req,res) {
-    var articleName=reg.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article-one',function (req,res) {
+    
+    res.send(createTemplate(article-one));
 });
 
 
