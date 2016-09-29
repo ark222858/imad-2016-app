d69bf4831ad363fae489ecdf762e48ec86bab110
ark222858 <ark222858@gmail.com>
2016-09-28 20:18:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of the content
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//make the image move
+var element=document.getElementById('modi');
+img.onclick=function(){
+    img=style.marginleft='100px';
+};
