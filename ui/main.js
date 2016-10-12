577561a65b427d293949d98a01039f29567f416c
ark222858 <ark222858@gmail.com>
2016-10-11 22:58:12 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,16 +28,30 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    //make the request to the server and send the names
-    //capture a list of name and render it as a list
-    var names=['name1','name2','name3'];
-    var list="";
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
     
+     var request=new XMLHttpRequest();
     
-     
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //take some action
+            if(request.status==200){
+                //make the request to the server and send the names
+                //capture a list of name and render it as a list
+                var names=request.responseText;
+                names=JSON.parse(names); 
+                var list="";
+                for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+            
+        }
+    };
+     //make a request to cunter endpoint
+     request.open('GET','http://ark222858.imad.hasura-app.io/submit-name?name='+ name,true);
+     request.send(null);
+   
 };
