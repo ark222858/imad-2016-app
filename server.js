0a3b3ca2e13a635ef0e2c12251022967489f94ee
ark222858 <ark222858@gmail.com>
2016-10-11 22:50:29 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,9 +114,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){ //url:/submit-name?name=xxxx
     //get the name from request object
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     //JSON javascript object notation 
     res.send(JSON.stringify(names));
